3102a79f8131681c4fab104641566b6e4a7dcea6
skye <skye53653@gmail.com>
2017-09-22 17:15:38 +0900

    finish part of the crawler

---
+++ ptt/ptt/spiders/ptt_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+
+class PTTSpider(scrapy.Spider):
+	name = "ptt"
+
+	def start_requests(self):
+		urls = ['https://www.ptt.cc/bbs/lesbian/index.html']
+		for url in urls:
+			yield scrapy.Request(url=url,callback=self.parse)
+
+	def parse(self,response):
+		#data = response.css('title::text').extract()[0]
+		articles = response.css("div.r-ent")
+		for article in articles:
+			# get title
+			title = article.css("div.title")
+			title = title.css("a::text").extract_first()
+			# get date
+			meta = article.css("div.meta")
+			date = meta.css("div.date::text").extract_first()
+			# get author
+			author = meta.css("div.author::text").extract_first()
+
+			print(title,date,author)
+			
+			# get url
+			title = article.css("div.title")
+			url = title.css("a::attr(href)").extract_first()
+			url = 'https://www.ptt.cc' + url
+			content = scrapy.Request(url, callback=self.parse_content)
+			print(type(content))
+		#with open("/Users/skye/result.txt","w") as f:
+		#	f.write(str(data))
+	
+	def parse_content(self,response):
+		pass
